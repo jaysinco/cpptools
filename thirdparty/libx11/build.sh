4ecbf88ebbc05929e589dab01a6c7f0a9f2bdf84
jaysinco <jaysinco@163.com>
2023-12-21 14:22:48 +0800

    add x11

---
+++ thirdparty/libx11/build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+
+do_clean=0
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+         -c) do_clean=1 && shift ;;
+          *) echo "Unknown option: $1" && exit 1 ;;
+    esac
+done
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source $TC_TOOLCHAIN_DIR/env.sh
+SOURCE_DIR=libX11-1.8.7
+
+if [ $do_clean -eq 1 ]; then
+    rm -rf $SCRIPT_DIR/src
+    rm -rf $SCRIPT_DIR/out
+fi
+
+if [ ! -d $SCRIPT_DIR/src/$SOURCE_DIR ]; then
+    mkdir -p $SCRIPT_DIR/src
+    tar -xf $TC_SOURCE_REPO/$SOURCE_DIR.tar.xz -C $SCRIPT_DIR/src
+fi
+
+mkdir -p $SCRIPT_DIR/out \
+&& \
+pushd $SCRIPT_DIR/out \
+&& \
+../src/$SOURCE_DIR/configure \
+    --with-sysroot=$TC_SYSROOT \
+    --prefix $TC_INSTALL_DIR \
+    --host=$TC_COMPILER_TUPLE \
+    --build=$TC_HOST_COMPILER_TUPLE \
+    --enable-shared \
+    --disable-static \
+    --enable-malloc0returnsnull \
+&& \
+make -j`nproc` \
+&& \
+make install
---
+++ thirdparty/libxcb/build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+do_clean=0
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+         -c) do_clean=1 && shift ;;
+          *) echo "Unknown option: $1" && exit 1 ;;
+    esac
+done
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source $TC_TOOLCHAIN_DIR/env.sh
+SOURCE_DIR=libxcb-1.16
+
+if [ $do_clean -eq 1 ]; then
+    rm -rf $SCRIPT_DIR/src
+    rm -rf $SCRIPT_DIR/out
+fi
+
+if [ ! -d $SCRIPT_DIR/src/$SOURCE_DIR ]; then
+    mkdir -p $SCRIPT_DIR/src
+    tar -xf $TC_SOURCE_REPO/$SOURCE_DIR.tar.xz -C $SCRIPT_DIR/src
+fi
+
+export PKG_CONFIG_PATH=$TC_INSTALL_DIR/share/pkgconfig
+
+mkdir -p $SCRIPT_DIR/out \
+&& \
+pushd $SCRIPT_DIR/out \
+&& \
+../src/$SOURCE_DIR/configure \
+    --with-sysroot=$TC_SYSROOT \
+    --prefix $TC_INSTALL_DIR \
+    --host=$TC_COMPILER_TUPLE \
+    --build=$TC_HOST_COMPILER_TUPLE \
+    --enable-shared \
+    --disable-static \
+&& \
+make -j`nproc` \
+&& \
+make install
