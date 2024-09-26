fed6cac1d33143aecc6a79ac9f454e3ab37b67c6
jaysinco <jaysinco@163.com>
2024-09-26 14:23:14 +0800

    tmux set default layout

--- etc/sway/launch.sh
+++ etc/sway/launch.sh
@@ -2,5 +2,5 @@
 
 swaymsg "layout tabbed"
 sleep 0.3s
-swaymsg "exec /usr/bin/foot"
+swaymsg "exec /usr/bin/foot /usr/bin/tmux new \; splitw -v \; neww \; splitw -v \; splitw -h \; selectw -t 0\;"
 sleep 0.3s
