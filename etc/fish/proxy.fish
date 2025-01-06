330ae77bfc10a26b4d9dbe66e1cf8c40011aaecc
jaysinco <jaysinco@163.com>
2025-01-06 14:03:14 +0800

    improve proxy fish

--- etc/fish/proxy.fish
+++ etc/fish/proxy.fish
@@ -2,5 +2,10 @@ set -gx http_proxy "http://127.0.0.1:7890"
 set -gx https_proxy "http://127.0.0.1:7890"
 set -gx ftp_proxy "http://127.0.0.1:7890"
 
-clash >/dev/null 2>&1 &
-microsoft-edge-stable >/dev/null 2>&1 &
+if not pgrep -f "clash" > /dev/null
+    nohup clash >/dev/null 2>&1 &
+end
+
+if not pgrep -f "msedge" > /dev/null
+    microsoft-edge-stable >/dev/null 2>&1 &
+end
