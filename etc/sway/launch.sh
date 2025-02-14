46b08b482ffbf8317f9d241b53e9e45e649c7e52
jaysinco <jaysinco@163.com>
2025-02-14 14:27:24 +0800

    change tmux layout

--- etc/sway/launch.sh
+++ etc/sway/launch.sh
@@ -2,5 +2,5 @@
 
 swaymsg "layout tabbed"
 sleep 0.3s
-swaymsg "exec /usr/bin/foot /usr/bin/tmux new \; splitw -v \; splitw -h \; splitw -v \; selectp -t 0\;"
+swaymsg "exec /usr/bin/foot /usr/bin/tmux new \; splitw -v \; selectp -t 0 \; splitw -h \; selectp -t 2 \; splitw -h \; splitw -v \; selectp -t 0"
 sleep 0.3s
