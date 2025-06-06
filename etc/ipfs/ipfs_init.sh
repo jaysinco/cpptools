46a9b646f91d9f0ad945266f2a7655bcbef324c9
jaysinco <jaysinco@163.com>
2025-06-06 11:21:20 +0800

    add static relay

--- etc/ipfs/ipfs_init.sh
+++ etc/ipfs/ipfs_init.sh
@@ -10,6 +10,7 @@ jq '.Identity.PrivKey="CAESQIksHEvxTAU+xZ/Ohm05CCRL26lyCty/jUlmbFbkMGS2U138FbS4D
 ipfs config --json Identity.PeerID '"12D3KooWFRo8PmgF6RiSsZRyTsNS7fghhuVyHHoNktJtndA3uair"'
 
 # peers
+ipfs config --json Swarm.RelayClient.StaticRelays '["/ip4/139.224.196.187/tcp/4001/p2p/12D3KooWFRo8PmgF6RiSsZRyTsNS7fghhuVyHHoNktJtndA3uair"]'
 ipfs config --json Peering.Peers '[{"ID": "12D3KooWFRo8PmgF6RiSsZRyTsNS7fghhuVyHHoNktJtndA3uair", "Addrs": ["/ip4/139.224.196.187/tcp/4001"]}]'
 ipfs config --json Peering.Peers '[{"ID": "12D3KooWHgSc6JyTMXZR9phywKPkVhyfXX7Gxv58usTQ6ZbCBo6e"}]'
 
