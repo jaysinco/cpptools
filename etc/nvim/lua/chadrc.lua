b6793a54ed67f0c7d3171a473705ff1bd644a22f
jaysinco <jaysinco@163.com>
2025-06-13 15:04:36 +0800

    fix nvim theme

--- etc/nvim/lua/chadrc.lua
+++ etc/nvim/lua/chadrc.lua
@@ -7,6 +7,7 @@ local M = {}
 
 M.base46 = {
 	theme = "vscode_dark",
+	theme_toggle = { "vscode_dark", "vscode_light" },
 
 	-- hl_override = {
 	-- 	Comment = { italic = true },
@@ -16,11 +17,11 @@ M.base46 = {
 
 -- M.nvdash = { load_on_startup = true }
 
--- M.ui = {
--- 	tabufline = {
--- 		lazyload = false
--- 	}
--- }
+M.ui = {
+	tabufline = {
+		lazyload = false
+	}
+}
 
 M.cheatsheet = {
     theme = "simple", -- simple/grid
