41eb80df754a5ec1b02a19c101ed5c07b0db9372
jaysinco <jaysinco@163.com>
2025-06-17 09:35:27 +0800

    disable inline diagnostics

--- etc/nvim/lua/configs/lspconfig.lua
+++ etc/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,10 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { 'clangd' }
+local servers = { "clangd" }
 vim.lsp.enable(servers)
+
+vim.diagnostic.config {
+  virtual_text = false, -- disable inline diagnostics
+}
 
 -- read :h vim.lsp.config for changing options of lsp servers
