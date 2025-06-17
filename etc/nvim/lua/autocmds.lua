c60941b28b22650e50280a1c8604de881e0a5713
jaysinco <jaysinco@163.com>
2025-06-17 14:59:14 +0800

    add autoformat

--- etc/nvim/lua/autocmds.lua
+++ etc/nvim/lua/autocmds.lua
@@ -21,3 +21,10 @@ vim.api.nvim_create_autocmd("VimEnter", {
     end
   end,
 })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  buffer = buffer,
+  callback = function()
+    vim.lsp.buf.format { async = false }
+  end
+})
