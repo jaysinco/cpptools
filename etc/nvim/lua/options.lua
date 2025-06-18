b26d29540d1e873ea944d527465b962fb20a911c
jaysinco <jaysinco@163.com>
2025-06-18 11:26:45 +0800

    set wrap false

--- etc/nvim/lua/options.lua
+++ etc/nvim/lua/options.lua
@@ -1,10 +1,11 @@
 require "nvchad.options"
 
 -- add yours here!
-vim.o.cursorlineopt = 'both' -- to enable cursorline!
-vim.o.tabstop = 4       -- How many spaces a tab counts for
-vim.o.shiftwidth = 4    -- Number of spaces for auto-indent
-vim.o.softtabstop = 4   -- Number of spaces for <Tab> and <BS> in insert mode
-vim.o.expandtab = true  -- Use spaces instead of tabs
+vim.o.cursorlineopt = "both" -- to enable cursorline!
+vim.o.tabstop = 4 -- How many spaces a tab counts for
+vim.o.shiftwidth = 4 -- Number of spaces for auto-indent
+vim.o.softtabstop = 4 -- Number of spaces for <Tab> and <BS> in insert mode
+vim.o.expandtab = true -- Use spaces instead of tabs
 vim.o.fixeol = false -- does not add a final newline
-vim.o.winborder = 'single'
+vim.o.winborder = "single"
+vim.o.wrap = false
