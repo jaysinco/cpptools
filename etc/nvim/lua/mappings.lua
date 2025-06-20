d9ec1707169ef6fef607d0bde354a5cbe724567a
jaysinco <jaysinco@163.com>
2025-06-20 16:11:03 +0800

    force buffer delete

--- etc/nvim/lua/mappings.lua
+++ etc/nvim/lua/mappings.lua
@@ -13,7 +13,7 @@ map('n', '<leader>X', function()
   local current = vim.api.nvim_get_current_buf()
   for _, buf in ipairs(vim.api.nvim_list_bufs()) do
     if buf ~= current and vim.api.nvim_buf_is_loaded(buf) then
-      vim.api.nvim_buf_delete(buf, {})
+      vim.api.nvim_buf_delete(buf, { force = true })
     end
   end
   require("nvim-tree.api").tree.open({ focus = false })
--- etc/nvim/lua/plugins/init.lua
+++ etc/nvim/lua/plugins/init.lua
@@ -19,6 +19,8 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
+      highlight = { enable = true },
+      indent = { enable = true },
       ensure_installed = {
         "vim",
         "lua",
